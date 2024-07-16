47dacfe56ee5e3b6d8375a2e0edf87847e7692f5
LAmourLesBaguettes <154582103+LAmourLesBaguettes@users.noreply.github.com>
2024-07-16 19:34:56 +0900

    Title: 숫자들의 배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240716/숫자들의 배수/multiple-of-numbers.py
@@ -0,0 +1,17 @@
+def print_multiples(n):
+    count_five_multiples = 0
+    multiple = 1
+    result = []
+
+
+    while count_five_multiples < 2:
+        value = n * multiple
+        result.apppend(value)
+        if value % 5 === 0:
+            count_five_multiples += 1
+        multiple += 1
+
+    print(" ".join(map(str, result)))
+
+n = int(input())
+print_multiples(n)
