3c5d2d8ab829a1f881187e184398eeb132dbbc9c
LAmourLesBaguettes <154582103+LAmourLesBaguettes@users.noreply.github.com>
2024-07-16 21:42:47 +0900

    Title: 십의 자리 숫자, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240716/십의 자리 숫자/number-of-tens-digit.py
+++ 240716/십의 자리 숫자/number-of-tens-digit.py
@@ -1,19 +1,15 @@
-def count_tens_place(numbers):
-    counts = [0] * 10
-    for number in numbers:
-        if number == 0:
-            continue
-        tens_place = number // 10
-        if tens_place > 0:
-            counts[tens_place] += 1
-    return counts[1:]  # we skip the first element (index 0) as we don't need the count for 0
+# 배열에 주어진 수를 입력받아 저장합니다.
+arr = list(map(int, input().split()))
+count_arr = [0] * 10
 
-# Read input values
-numbers = list(map(int, input().split()))
-
-# Get the counts of each tens place digit
-result = count_tens_place(numbers)
-
-# Print the results in the specified format
+# 카운팅 배열을 통해 십의 자리수 각각의 빈도 저장, 0이 나오면 for문에서 빠져나오기
+for elem in arr:
+	if elem == 0:
+		break
+	if elem < 10:
+		continue
+	count_arr[elem // 10] += 1
+	
+# 1부터 9까지 나온 횟수를 출력
 for i in range(1, 10):
-    print(f"{i} - {result[i-1]}")
+	print(f"{i} - {count_arr[i]}")
