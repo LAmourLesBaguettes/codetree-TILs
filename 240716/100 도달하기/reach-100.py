6e6aa08d9e658ff4fe2ec7cbec158bc21418ca0c
LAmourLesBaguettes <154582103+LAmourLesBaguettes@users.noreply.github.com>
2024-07-16 20:23:12 +0900

    Title: 100 도달하기, Time: 132ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240716/100 도달하기/reach-100.py
+++ 240716/100 도달하기/reach-100.py
@@ -1,10 +1,10 @@
 def generate_sequence(n):
-    sequence = [1, n]
+    sequence = [1, n]  # 첫 번째 항은 1, 두 번째 항은 n으로 초기화
     while True:
-        next_value = sequence[-1] + sequence[-2]
-        if next_value > 120:
+        next_value = sequence[-1] + sequence[-2]  # 다음 항은 전전항과 전항의 합
+        if next_value > 100:  # 다음 값이 100을 넘으면 종료
             break
-        sequence.append(next_value)
+        sequence.append(next_value)  # 리스트에 다음 값을 추가
     return sequence
 
 # 입력 받기
