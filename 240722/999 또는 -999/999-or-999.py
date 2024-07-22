73b28a1fc317a079c98094f1e5fd83bba43cd9a3
LAmourLesBaguettes <154582103+LAmourLesBaguettes@users.noreply.github.com>
2024-07-22 20:21:41 +0900

    Title: 999 또는 -999, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240722/999 또는 -999/999-or-999.py
@@ -0,0 +1,14 @@
+numbers = list(map(int, input().split()))
+
+min_number = -1000
+max_number = 1000
+
+for number in numbers:
+    if number == 999 or number == -999:
+        break
+    if number > max_number:
+    max_number = number
+    if number < min_number:
+    min_number = number
+
+print(max_number, min_number)
