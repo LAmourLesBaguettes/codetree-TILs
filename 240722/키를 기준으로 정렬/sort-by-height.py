30a5a7a302219adab1fd17efd79c3bef61e26244
LAmourLesBaguettes <154582103+LAmourLesBaguettes@users.noreply.github.com>
2024-07-22 21:32:53 +0900

    Title: 키를 기준으로 정렬, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240722/키를 기준으로 정렬/sort-by-height.py
@@ -0,0 +1,14 @@
+n = int(input().strip())
+
+people = []
+
+for - in range(n):
+    name, height, weight = input().strip().split()
+    height = int(height)
+    weight = int(weight)
+    people.append((name, height, weight))
+
+people.sort(key = lamda x: x[1])
+
+for person in people:
+    print(person[0], person[1], person[2])
