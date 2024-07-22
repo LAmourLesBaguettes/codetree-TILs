9ecb0a19946b111a9536e512b903158d322be0f6
LAmourLesBaguettes <154582103+LAmourLesBaguettes@users.noreply.github.com>
2024-07-22 21:33:49 +0900

    Title: 키를 기준으로 정렬, Time: 214ms, Memory: 24MB, Status: Passed - CodeTree

--- 240722/키를 기준으로 정렬/sort-by-height.py
+++ 240722/키를 기준으로 정렬/sort-by-height.py
@@ -8,7 +8,7 @@ for _ in range(n):
     weight = int(weight)
     people.append((name, height, weight))
 
-people.sort(key = lamda x: x[1])
+people.sort(key = lambda x: x[1])
 
 for person in people:
     print(person[0], person[1], person[2])
