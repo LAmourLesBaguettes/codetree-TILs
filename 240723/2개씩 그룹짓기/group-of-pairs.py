83b69f1b3dbfc8f562c560f8bc38651e2a0b4990
LAmourLesBaguettes <154582103+LAmourLesBaguettes@users.noreply.github.com>
2024-07-23 11:58:05 +0900

    Title: 2개씩 그룹짓기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240723/2개씩 그룹짓기/group-of-pairs.py
@@ -0,0 +1,13 @@
+N = int(input())
+numbers = list(map(int, input().split))
+
+numbers.sort()
+
+max_sum = 0
+
+for i in range(N):
+    group_sum = numbers[i] + numbers[2*N - 1 - i]
+    max_sum = max(max_sum, group_sum)
+
+
+print(max_sum)
