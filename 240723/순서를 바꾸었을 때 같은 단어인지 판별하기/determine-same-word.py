1bdeecdad6f7a94bc4c3be96571e07ca455f1bc6
LAmourLesBaguettes <154582103+LAmourLesBaguettes@users.noreply.github.com>
2024-07-23 12:00:29 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240723/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.py
@@ -0,0 +1,12 @@
+from collection import Counter
+
+word1 = input().strip()
+word2 = input().strip()
+
+counter1 = Counter(word1)
+counter2 = Counter(word2)
+
+if counter1 == counter2:
+    print("Yes")
+else:
+    print("No")
