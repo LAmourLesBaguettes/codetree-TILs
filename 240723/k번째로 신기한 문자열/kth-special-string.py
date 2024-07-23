1a9192c647ccedfbd25f35a9f5414f38651f9d5e
LAmourLesBaguettes <154582103+LAmourLesBaguettes@users.noreply.github.com>
2024-07-23 12:04:09 +0900

    Title: k번째로 신기한 문자열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240723/k번째로 신기한 문자열/kth-special-string.py
@@ -0,0 +1,14 @@
+n ,k, T = input().split()
+n = int(n)
+k = int(k)
+
+words = []
+for _ in range():
+    word = input().strip()
+    words.append(word)
+
+filtered_words = [word for word in words if word.startswith(T)]
+
+filtered_words.sort()
+
+print(filtered_words[k-1])
