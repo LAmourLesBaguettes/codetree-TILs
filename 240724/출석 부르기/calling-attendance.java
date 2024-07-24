81943df71024db4b59965ab9d2d7bac75cd85478
LAmourLesBaguettes <154582103+LAmourLesBaguettes@users.noreply.github.com>
2024-07-24 13:24:10 +0900

    Title: 출석 부르기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240724/출석 부르기/calling-attendance.java
+++ 240724/출석 부르기/calling-attendance.java
@@ -2,15 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner = scanner = new Scanner(System.in);
-        int number = scanner.next.Int();
-
-        String students = {"John", "Tom", "Paul"};
-
-        if(number >= 1 && number <= students.length){
-            System.out.println(students[number-1]);
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        
+        // 학생 이름을 저장하는 배열
+        String[] students = { "John", "Tom", "Paul" };
+        
+        // 출석번호에 해당하는 학생 이름 출력
+        if (number >= 1 && number <= students.length) {
+            System.out.println(students[number - 1]);
         } else {
             System.out.println("Vacancy");
         }
+        
+        scanner.close();
     }
 }
