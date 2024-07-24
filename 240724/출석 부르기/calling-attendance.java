77185e8ee70fe0fe03b58203b5585b7aebd1be01
LAmourLesBaguettes <154582103+LAmourLesBaguettes@users.noreply.github.com>
2024-07-24 13:18:56 +0900

    Title: 출석 부르기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240724/출석 부르기/calling-attendance.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner = scanner = new Scanner(System.in);
+        int number = scanner.next.Int();
+
+        String students = {"John", "Tom", "Paul"};
+
+        if(number >= 1 && number <= students.length){
+            System.out.println(students[number-1]);
+        } else {
+            System.out.println("Vacancy")
+        }
+    }
+}
