78db54977790c1f228ea1c0e8611ff807b19f464
LAmourLesBaguettes <154582103+LAmourLesBaguettes@users.noreply.github.com>
2024-07-26 17:16:39 +0900

    Title: Date to Date, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240726/Date to Date/date-to-date.py
@@ -0,0 +1,14 @@
+days_in_month = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+
+m1, d1, m2, d2 = map(int, in put().split())
+
+day_count = days_in_month[m1] - d1 + 1
+
+if m1 == m2:
+    day_count = d2 - d1 + 1
+else:
+    for month in range(m1 + 1, m2):
+        day_count += days_in_month[month]
+        day_count += d2
+
+print(day_count)
